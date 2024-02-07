b0058555ca7cccf6ed033dbe1e38dc9f3b44d226
ligang <ligang1@xsyxsc.com>
2024-02-07 17:51:03 +0800

    feat: watch

--- src/knowledge/ChapterFour/computed.js
+++ src/knowledge/ChapterFour/computed.js
@@ -116,3 +116,65 @@ function trigger(target, key) {
   });
 }
 
+/* computed 函数 */
+
+// function computed(getter) {
+//   // 把 getter 作为副作用函数，创建一个 lazy 的 effect
+//   const effectFn = effect(getter, {
+//     lazy: true,
+//   });
+
+//   const obj = {
+//     // 当读取 value 时才执行 effectFn
+//     get value() {
+//       return effectFn();
+//     },
+//   };
+
+//   return obj;
+// }
+// const sumRes = computed(() => obj.foo + obj.bar);
+// console.log(sumRes.value);
+
+/* 在 computed 实现缓存的功能 */
+
+function computed(getter) {
+  // value 用来缓存上一次计算的值
+  let value;
+  // dirty 标志，用来标识是否需要重新计算值，为 true 则意味着 "脏"，需要计算
+  let dirty = true;
+
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      dirty = true;
+      trigger(obj, "value");
+    },
+  });
+
+  const obj = {
+    get value() {
+      // 只有当 dirty 为 true 时，并将得到的值缓存在 value 中
+      if (dirty) {
+        value = effectFn();
+        // 将 dirty 设置为 false，下一次直接访问缓存值
+        dirty = false;
+      }
+      // 当读取 value 时，手动调用 track 函数进行追踪
+      track(obj, "value");
+      return value;
+    },
+  };
+  return obj;
+}
+
+const sumRes = computed(() => obj.foo + obj.bar);
+
+effect(() => {
+  // 在该副作用函数中读取 sumRes.value
+  console.log(sumRes.value);
+});
+
+obj.foo++;
+
+// console.log(sumRes.value);
--- src/knowledge/ChapterFour/watch.js
+++ src/knowledge/ChapterFour/watch.js
@@ -50,14 +50,12 @@ const bucket = new WeakMap();
 const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
-    console.log("getter!!!");
     track(target, key);
     return target[key];
   },
 
   // 拦截设置操作
   set(target, key, newVal) {
-    console.log("setter!!!");
     // 设置属性值
     target[key] = newVal;
     trigger(target, key);
@@ -116,3 +114,61 @@ function trigger(target, key) {
   });
 }
 
+/* watch 函数 */
+
+function watch(source, cb) {
+  // 定义 getter
+  let getter;
+  //  如果 source 是函数，说明用户传递的是 getter，所以直接把 source 赋值给 getter
+  if (typeof source === "function") {
+    getter = source;
+  } else {
+    // 否则按照原来的实现调用 traverse 递归地读取
+    getter = () => traverse(source);
+  }
+  // 定义旧值与新值
+  let oldValue, newValue;
+  // 使用 effect 注册副作用函数时，开启 lazy 选项，并把返回值存储到 effectFn 中以便后续手动调用
+  const effectFn = effect(() => getter(), {
+    lazy: true,
+    scheduler() {
+      // 在 scheduler 中重新执行副作用函数，得到的是新值
+      newValue = effectFn();
+      // 将旧值和新值作为回调函数的参数
+      cb(newValue, oldValue);
+      // 更新旧值，不然下一次会得到错误的旧值
+      oldValue = newValue;
+    },
+  });
+
+  // 手动调用副作用函数，拿到的值就是旧值
+  oldValue = effectFn();
+}
+
+function traverse(value, seen = new Set()) {
+  // 如果要读取的数据是原始值，或者已经被读取过了，那么什么都不做
+  if (typeof value !== "object" || value === null || seen.has(value)) return;
+  // 将数据添加到 seen 中，代表遍历地读取过了，避免循环引用引起的死循环
+  seen.add(value);
+  // 暂时不考虑数组等其他结构
+  // 假设 value 就是一个对象，使用 for...in 读取对象的每一个值，并递归地调用 traverse 进行处理
+  for (const k in value) {
+    traverse(value[k], seen);
+  }
+  console.log("source-value", value, seen);
+  return value;
+}
+
+// watch(obj, () => {
+//   console.log("数据变化了");
+// });
+
+/* watch函数除了可以观测响应式数据，还可以接收一个getter */
+watch(
+  () => obj.foo,
+  (newValue, oldValue) => {
+    console.log("数据变化了", newValue, oldValue);
+  }
+);
+
+obj.foo++;

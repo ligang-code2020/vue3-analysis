3aa487ecf493d22604144d7abc43317d1dab1278
ligang <ligang13874843373@gmail.com>
2024-09-29 15:49:24 +0800

    feat: 新增有限状态

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { tokenize } from './knowledge/ChapterFifteen/parser';
 
-
-const app = createApp(App)
+const app = createApp(App);
+// tokenize()
 
 // app.config.errorHandler = (err: Error, vm, info: string) => {
 //     // console.log("error",err);
 // }
 
-app.mount('#app')
+app.mount('#app');
 
 // createApp(App).mount('#app')
